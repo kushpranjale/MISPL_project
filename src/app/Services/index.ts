0a09d46da7dc74822b49062dc5acd85bba3a3e68
Kush pranjale <30398986+kushpranjale@users.noreply.github.com>
2020-08-19 09:50:10 +0530

    #kush update

--- src/app/Services/index.ts
+++ src/app/Services/index.ts
@@ -1,2 +1,2 @@
-export const url = "http://localhost:8080/api/";
-// export const url = "https://salty-beach-81043.herokuapp.com/api/";
+// export const url = "http://localhost:8080/api/";
+export const url = "https://salty-beach-81043.herokuapp.com/api/";
